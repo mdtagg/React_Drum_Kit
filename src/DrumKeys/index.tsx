7bd36807a5565ee2109a61846a6f6f44bd84cffe
Mike Tagg <mdtagg91@gmail.com>
2023-06-06 14:33:54 -0700

    drum pad buttons added and styled

--- src/DrumKeys/index.tsx
+++ src/DrumKeys/index.tsx
@@ -1,9 +1,18 @@
-
+import "./index.css"
 
 const DrumKeys = () => {
+
     return (
-        <div>
-            
+        <div className="key-container">
+            <div className="drum-pad">Q</div>
+            <div className="drum-pad">W</div>
+            <div className="drum-pad">E</div>
+            <div className="drum-pad">A</div>
+            <div className="drum-pad">S</div>
+            <div className="drum-pad">D</div>
+            <div className="drum-pad">Z</div>
+            <div className="drum-pad">X</div>
+            <div className="drum-pad">C</div>
         </div>
     )
 }
